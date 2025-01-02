7593dc0d1a76639f588c109bdc36124b66849a2f
Jatin Yadav <yadavshiv633@gmail.com>
2025-01-03 00:52:55 +0530

    New Changes

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const MainSection = () => {
     return (
         <>
-        <section className=''>
-            <div className='max-w-[1400px] mx-auto px-2'></div>
-        </section>
+            <section className='bg-BG1 h-screen bg-no-repeat bg-cover bg-center relative'>
+                <div className='absolute inset-0 bg-gradient-to-r from-black/70 via-black/50 to-gray-200/30' />
+                <div className='max-w-[1400px] mx-auto px-2 relative z-10'>
+                    <h3 className='text-5xl uppercase'>We make <br />what you want</h3>
+                </div>
+            </section>
         </>
-    )
+    );
 }
 
 export default MainSection;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,12 +12,12 @@ export default {
         foreground: "var(--foreground)",
       },
       backgroundImage:{
-        BG1: "url('/images/main-section/main.webp')",
-        BG2: "url('/images/main-section/main-1.webp')",
-        BG3: "url('/images/main-section/main-2.webp')",
-        BG4: "url('/images/main-section/main-3.webp')",
-        BG5: "url('/images/main-section/main-4.webp')",
-        BG6: "url('/images/main-section/main-5.webp')",
+        BG1: "url('/image/main-section/main.webp')",
+        BG2: "url('/image/main-section/main-1.webp')",
+        BG3: "url('/image/main-section/main-2.webp')",
+        BG4: "url('/image/main-section/main-3.webp')",
+        BG5: "url('/image/main-section/main-4.webp')",
+        BG6: "url('/image/main-section/main-5.webp')",
       }
     },
   },
